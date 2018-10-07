35cb6089d1cd8d42fb7a0b4561ffeec5399ac60b
Himanshu Arora <himanshuarora323@gmail.com>
2018-10-07 12:04:23 +0530

    server changes

--- server.js
+++ server.js
@@ -1,89 +1,43 @@
-const http=require("http");
-
 let config=require("./config/config")
-const app=require("./app");
-const debug=require("debug")("node-angular");
-const socketIO=require("socket.io");
+const express = require("express");
+const bodyParser = require("body-parser")
+const tradeRoutes = require("./routes/trades")
 
-let socketInstance;
+const mongoose = require("mongoose");
 
-// let server=http.createServer((req,resp)=>{
-//     resp.end("This is ITTT")
-// })
 
-const normalizePort = val => {
-    var port = parseInt(val, 10);
-  
-    if (isNaN(port)) {
-      // named pipe
-      return val;
-    }
-  
-    if (port >= 0) {
-      // port number
-      return port;
-    }
-  
-    return false;
-  };
-  
-  const onError = error => {
-    if (error.syscall !== "listen") {
-      throw error;
-    }
-    const bind = typeof addr === "string" ? "pipe " + addr : "port " + port;
-    switch (error.code) {
-      case "EACCES":
-        console.error(bind + " requires elevated privileges");
-        process.exit(1);
-        break;
-      case "EADDRINUSE":
-        console.error(bind + " is already in use");
-        process.exit(1);
-        break;
-      default:
-        throw error;
-    }
-  };
-  
-  const onListening = () => {
-    const addr = server.address();
-    const bind = typeof addr === "string" ? "pipe " + addr : "port " + port;
-   console.log("Listening on " + bind);
-    
-  };
-  
-  const port = normalizePort(process.env.PORT || "3002");
-  app.set("port", port);
-
-let server=http.createServer(app)
-
-// let io= socketIO(server);
-
-// io.on("connection",(socket)=>{
-
-//   console.log("In");
-//   socketInstance=socket;
-
-//   socket.on('createTrade', () =>{
-                
-//     console.log("event occured");
-
-//     //console.log(data);
-
-//     io.emit("newTrade")
-    
-
-// })
-// })
+const path = require("path");
 
 
 
-server.on("error", onError);
-server.on("listening", onListening);
+const app = express();
+
+const port=process.env.PORT || "3002";
+
+mongoose.connect(process.env.MONGODB_URI);
 
 
-server.listen(port);
 
-module.exports={app}
+
+app.use(bodyParser.json())
+
+app.use("/", express.static(path.join(__dirname, "angular")))
+
+app.use((req, resp, next) => {
+
+    resp.setHeader("Access-Control-Allow-Origin", "*");
+
+
+    resp.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,Authorization");
+
+    resp.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE,OPTIONS,PUT")
+    next();
+})
+
+app.use("/api/trades", tradeRoutes)
+
+
+app.listen(port,()=>{
+    console.log(`Server is started on port ${port}`);
+})
 
